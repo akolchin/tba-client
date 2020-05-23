4f4d07bbae47b20160f0f057886c4b5a6d7dd635
akolchin@gmail.com <akolchin@gmail.com>
2020-05-23 21:57:14 +0300

    Add Material toolbar with the app name and a responsive side nav based on Material breakpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { SettingsComponent } from './settings/settings.component';
@@ -16,7 +23,13 @@ import { OverviewComponent } from './overview/overview.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
